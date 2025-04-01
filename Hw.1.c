569117a27d505b7cd0211c47d5b6d893fc11855a
Lin-K-un <u11302209@o365.ttu.edu.tw>
2025-04-01 19:23:11 +0800

    寫一個新的程式，20行個人風格的畫面

---
+++ Hw.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main (void)
+{
+	int i;
+	for(i=1;i<=5;i++)/*執行五次下面四行的程式*/ 
+	{
+		printf("o.O o.O o.O o.O o.O o.O o.O o.O o.O o.O o.O o.O o.O o.O o.O\n");
+		printf("O.o O.o O.o O.o O.o O.o O.o O.o O.o O.o O.o O.o O.o O.o O.o\n");
+		printf("O.O O.O O.O O.O O.O O.O O.O O.O O.O O.O O.O O.O O.O O.O O.O\n");
+		printf("o.o o.o o.o o.o o.o o.o o.o o.o o.o o.o o.o o.o o.o o.o o.o\n");
+	}
+	system("pause");
+	return 0; 
+} 
