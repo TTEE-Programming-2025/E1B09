ffd987ab23b6262cee7c9f0b6c8109f895531533
Lin-K-un <u11302209@o365.ttu.edu.tw>
2025-04-01 19:39:08 +0800

    個人畫面清除後，判斷密碼是否正確

--- Hw.1.c
+++ Hw.1.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 int main (void)
 {
-	int i;
+	int i,a;
 	for(i=1;i<=5;i++)/*執行五次下面四行的程式*/ 
 	{
 		printf("o.O o.O o.O o.O o.O o.O o.O o.O o.O o.O o.O o.O o.O o.O o.O\n");
@@ -10,6 +10,17 @@ int main (void)
 		printf("O.O O.O O.O O.O O.O O.O O.O O.O O.O O.O O.O O.O O.O O.O O.O\n");
 		printf("o.o o.o o.o o.o o.o o.o o.o o.o o.o o.o o.o o.o o.o o.o o.o\n");
 	}
-	system("pause");
+	system("pause");/*等待使用者輸入按鍵*/ 
+	system("CLS");/*清除螢幕*/ 
+	
+	printf("請輸入密碼:");
+	scanf("%d",&a);/* 輸入密碼*/ 
+	if(a==2025)/*判斷密碼是否正確*/ 
+	{
+		system("pause");/*等待使用者輸入按鍵*/ 
+		system("CLS");/*清除螢幕*/
+	}
+	else
+	printf("密碼錯誤\n");
 	return 0; 
 } 
