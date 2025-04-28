82dd110a5556177d69e7c3e928e9f85dc33d66ca
Lin-K-un <u11302209@o365.ttu.edu.tw>
2025-04-28 12:56:03 +0800

    新增密碼的判斷，密碼錯誤輸錯三次會結束程式

--- Hw.2.c
+++ Hw.2.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 int main(void)
 {
-	int i;
+	int i,wrong,password;
 	printf("*********************************\n");			//印出個人風格的畫面
 	for(i=1;i<=6;i++)
 	{	
@@ -11,6 +11,28 @@ int main(void)
 		printf("| /(Q m Q)/ /(Q m Q)/ /(Q m Q)/ |\n");
 	}
 	printf("***********************************\n");
+	
+		printf("密碼是2025\n");
+	
+	do												    	//密碼判斷 
+	{
+		if(wrong==3)										//當wrong=3時代表密碼錯誤三次將結束此程式
+		{
+			printf("抱歉密碼已經錯誤三次，將結束此程式"); 
+			return 0;
+		}
+		
+		printf("請輸入密碼:");
+		scanf("%d",&password);
+		if(password==2025)
+		{
+			break;
+		}
+		
+		printf("密碼錯誤請重新輸入\n");
+		wrong++;									    	//密碼錯誤時此wrong變數+1 
+	
+	}while(1);
 	system("pause");
 	return 0;
 }
