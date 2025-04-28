5a10ce00ebb4618bb4f15a8bc5d29a81d19ba28b
Lin-K-un <u11302209@o365.ttu.edu.tw>
2025-04-28 12:33:56 +0800

    第一次繳交，印出個人風格的畫面

---
+++ Hw.2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	int i;
+	printf("*********************************\n");			//印出個人風格的畫面
+	for(i=1;i<=6;i++)
+	{	
+		printf("| \\(> _ <)\\ \\(> _ <)\\ \\(> _ <)\\ |\n");
+		printf("| \\(．w．)/ \\(．w．)/ \\(．w．)/ |\n");
+		printf("| /(Q m Q)/ /(Q m Q)/ /(Q m Q)/ |\n");
+	}
+	printf("***********************************\n");
+	system("pause");
+	return 0;
+}
