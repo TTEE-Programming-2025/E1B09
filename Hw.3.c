d610e7b7937c173274fb1d82f084617ef97983eb
Lin-K-un <u11302209@o365.ttu.edu.tw>
2025-05-11 23:10:50 +0800

    Check if the password is correct

--- Hw.3.c
+++ Hw.3.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 int main(void)
 {
-	int i,j;
+	int i,j,wrong=0,password;
 	
 	printf("**********************************************\n");			//Print a personalized screen display
     for (i=0;i<20;i++) {
@@ -18,6 +18,27 @@ int main(void)
         } 
 	printf("**********************************************\n");
 	
+	printf("歡迎~\n");
+	
+	do			//密碼判斷 
+	{
+		if(wrong==3)			//當wrong=3時表示密碼錯誤三次將結束此程式
+		{
+			printf("抱歉密碼已經錯誤三次，將結束此程式"); 
+			return 0;
+		}
+		
+		printf("請輸入四個數字的密碼:");
+		scanf("%d",&password);
+		if(password==2025)
+		{
+			break;
+		}
+		
+		printf("密碼錯誤請重新輸入\n");
+		wrong++;									    	//密碼錯誤時此wrong變數+1 
+	
+	}while(1);
 
 	system("pause");
 	return 0;
