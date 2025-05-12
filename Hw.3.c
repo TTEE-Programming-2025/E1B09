334d34ec556cdfe9b58bf4a0a2814450eb172a9a
Lin-K-un <u11302209@o365.ttu.edu.tw>
2025-05-12 22:04:58 +0800

    Turn the Chinese into English.

--- Hw.3.c
+++ Hw.3.c
@@ -23,24 +23,24 @@ int main(void)
         } 
 	printf("**********************************************\n");
 	
-	printf("歡迎~\n");
+	printf("Welcome~\n");
 	
 	do			//Password verification loop
 	{
 		if(wrong==3)			//If wrong = 3, indicating 3 incorrect password attempts, the program will end
 		{
-			printf("抱歉密碼已經錯誤三次，將結束此程式"); 
+			printf("Sorry, the password was entered incorrectly three times. The program will exit."); 
 			return 0;
 		}
 		
-		printf("請輸入四個數字的密碼:");
+		printf("Please enter a 4-digit password:");
 		scanf("%d",&password);
 		if(password==2025)			//If password is correct, break the loop
 		{
 			break;
 		}
 		
-		printf("密碼錯誤請重新輸入\n");
+		printf("Incorrect password, please try again\n");
 		wrong++;			//密Increase wrong variable by 1 on incorrect password 
 	
 	}while(1);
@@ -75,7 +75,7 @@ int main(void)
 		printf("| d. Exit                          |\n");
 		printf("------------------------------------\n");
 		
-		printf("請輸入你要選擇的選項:");
+		printf("Please enter your option:");
 		fflush(stdin);
 		scanf("%c",&option);			//Input option
 		//Option a
@@ -87,7 +87,7 @@ int main(void)
 		//Option b
 		if(option=='b')
 		{
-			printf("請問您需要幾個座位？(1-4):");
+			printf("How many seats do you need?(1-4):");
             fflush(stdin);
             scanf("%d",&b1);			//Input the number of seats needed
             
@@ -179,7 +179,7 @@ int main(void)
 					showseats(seats);
 				}
 			}
-			printf("是否要保留(y/n):");			//Ask if they want to keep the seats
+			printf("Do you want to reserve the seats? (y/n):");			//Ask if they want to keep the seats
 			fflush(stdin);
 			scanf("%c",&b5);
 			if(b5=='y'||b5=='Y')			//Keep the reservation
@@ -214,24 +214,24 @@ int main(void)
 		//Option c
 		if(option=='c')
 		{
-			printf("請輸入有幾位(1-71):");
+			printf("How many people? (1-71):");
 			fflush(stdin);
 			scanf("%d",&num);
 			for(i=1;i<=num;i++)
 			{
 				while(1)
 				{
-					printf("請依照範例依序輸入第%d個座位(1-1為第一列的第一行):",i);
+					printf("Please enter seat %d in the format Example (e.g., 1 - 1 for row 1, column 1):",i);
 					fflush(stdin);
 					scanf("%d %c %d",&c1,&c2,&c3);
 					if(c2!='-')
 					{
-						printf("格式錯誤，請輸入正確格式\n");
+						printf("Invalid format. Please use the correct format.\n");
 						continue;
 					}
 					if(seats[c1-1][c3-1]=='*')
 					{
-						printf("錯誤，這位子已有人，麻煩重新選擇\n");
+						printf("Error, seat already taken. Please choose another.\n");
 						continue;
 					}
 					if(c2=='-'&&seats[c1-1][c3-1]=='-')
@@ -243,7 +243,7 @@ int main(void)
 					
 			}
 			showseats(seats);
-					printf("沒有問題的話按下任意按鍵繼續\n");
+					printf("If everything is correct, press any key to continue.\n");
 					system("pause");
 					for(i=0;i<=8;i++)
 					{
@@ -268,7 +268,7 @@ int main(void)
 				{
 					break;
 				}
-				printf("輸入錯誤，請輸入y或n\n"); 
+				printf("Invalid input. Please enter y or n.\n"); 
 				
 			}while(1);
 			
